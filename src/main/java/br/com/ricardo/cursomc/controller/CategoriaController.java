23c8097032605b3dc4d156ffbe1590a11b7c151d
ricardo.souza <ricardo.souza@iteris.com.br>
2018-12-19 11:44:35 -0200

    Testando a primeira classe de dominio

---
+++ src/main/java/br/com/ricardo/cursomc/controller/CategoriaController.java
@@ -0,0 +1,29 @@
+package br.com.ricardo.cursomc.controller;
+
+import br.com.ricardo.cursomc.model.Categoria;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2,"Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+
+        return lista;
+
+    }
+}
