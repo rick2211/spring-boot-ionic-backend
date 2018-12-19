d5ace63f92ca2fa2557c9c3e209f30fb2a2785c7
ricardo.souza <ricardo.souza@iteris.com.br>
2018-12-19 11:58:59 -0200

    Banco H2 e criacao de base de dados

--- src/main/java/br/com/ricardo/cursomc/model/Categoria.java
+++ src/main/java/br/com/ricardo/cursomc/model/Categoria.java
@@ -5,18 +5,22 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@Entity
 public class Categoria implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column
     private String nome;
 
 }
