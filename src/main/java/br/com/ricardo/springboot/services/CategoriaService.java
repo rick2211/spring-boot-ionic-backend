0ed7bf32a6774c59d977cc9ca6dc958fe06d0d41
ricardo.souza <ricardo.souza@iteris.com.br>
2018-12-27 14:52:41 -0200

    Busca opcional paginada de Categoria

--- src/main/java/br/com/ricardo/springboot/services/CategoriaService.java
+++ src/main/java/br/com/ricardo/springboot/services/CategoriaService.java
@@ -6,6 +6,9 @@ import br.com.ricardo.springboot.services.exceptions.DataIntegrityException;
 import br.com.ricardo.springboot.services.exceptions.ObjetctNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -53,5 +56,10 @@ public class CategoriaService {
         return categoriaRepository.findAll();
 
 
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return categoriaRepository.findAll(pageRequest);
     }
 }
