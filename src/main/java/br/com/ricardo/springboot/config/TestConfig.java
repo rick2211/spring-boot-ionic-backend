8e640455c8e96575cb7546c6d2e0bdad66f198f5
ricardo.souza <ricardo.souza@iteris.com.br>
2019-01-03 14:41:30 -0200

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/br/com/ricardo/springboot/config/TestConfig.java
+++ src/main/java/br/com/ricardo/springboot/config/TestConfig.java
@@ -1,10 +1,7 @@
 package br.com.ricardo.springboot.config;
 
 import br.com.ricardo.springboot.services.DBService;
-import br.com.ricardo.springboot.services.EmailService;
-import br.com.ricardo.springboot.services.SmtpEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,28 +9,15 @@ import org.springframework.context.annotation.Profile;
 import java.text.ParseException;
 
 @Configuration
-@Profile("dev")
-public class DevConfig {
+@Profile("test")
+public class TestConfig {
 
     @Autowired
     private DBService dbService;
 
-    @Value("${spring.jpa.hibernate.ddl-auto}")
-    private String strategy;
-
     @Bean
     public boolean instantiateDatabase() throws ParseException {
-
-        if (!"create".equals(strategy)) {
-            return false;
-        }
-
         dbService.instantiateTestDataBase();
         return true;
-    }
-
-    @Bean
-    public EmailService emailService() {
-        return new SmtpEmailService();
     }
 }
